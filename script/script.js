1b12ed37105404cb5982ea53bd005ba53ec334cf
Mrpeej <rsantosdasilva05@gmail.com>
2023-10-04 16:21:39 -0300

    segunda alteração

--- script/script.js
+++ script/script.js
@@ -23,3 +23,41 @@ function fazM()
         document.getElementById("resultado").style.backgroundColor="lightcoral"
     }
 }
+
+// -------------------------------------------------------------- //
+
+function fazM2()
+{
+    let nota1 =parseFloat(document.getElementById("nota1").value);
+    let nota2 =parseFloat(document.getElementById("nota2").value);
+    let falta =parseInt(document.getElementById("falta").value);
+
+    let media
+
+    media= (nota1 + nota2)/2
+
+    document.getElementById("media").innerHTML=media;
+
+    if(media>=7 && falta<=5){
+
+        document.getElementById("resultado").innerHTML="APROVADO"
+        document.getElementById("resultado").style.color="green"
+        document.getElementById("resultado").style.borderColor="black"
+        document.getElementById("resultado").style.backgroundColor="lightgreen"
+    }
+    
+    else if(falta>5){
+
+        document.getElementById("resultado").innerHTML="RECUPERAÇÃO"
+        document.getElementById("resultado").style.borderColor="black"
+        document.getElementById("resultado").style.backgroundColor="lightgoldenrodyellow"
+
+    }
+
+    else{
+        document.getElementById("resultado").innerHTML="REPROVADO"
+        document.getElementById("resultado").style.color="red"
+        document.getElementById("resultado").style.borderColor="black"
+        document.getElementById("resultado").style.backgroundColor="lightcoral"
+    }
+}
